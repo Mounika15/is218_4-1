6caafb0dfdf94e212431c2416ef5ba4fb002701c
mounika guntupalli <mg577@afsconnect1.njit.edu>
2017-02-26 22:26:23 -0500

    change name of category input

---
+++ add_category_form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Guitar Shop</title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<header><h1>Category Manager</h1></header>
+
+<main>
+	<h1>Add Category</h1>
+	<form action="add_category.php" method="post"id="add_category_form">
+	<label>Category:</label>
+	<input type="text" name="categoryName"><br>
+	<label>&nbsp;</label><br>
+	<input type="submit" value="Add Category"><br>
+	</form>
+	<p><a href="index.php">View Product List</a></p>
+
+</main>
+</body>
+</html>
